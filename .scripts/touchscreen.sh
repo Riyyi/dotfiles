c27d9555fe988995f1e66a256ca6255b559b279d
Riyyi <riyyi3@gmail.com>
2018-02-07 13:16:12 +0100

    Fix touchscreen toggle script

--- .scripts/touchscreen.sh
+++ .scripts/touchscreen.sh
@@ -4,8 +4,8 @@ device="04F3200A:00 04F3:2373"
 state=$(xinput --list-props "$device" | grep "Device Enabled" | sed -nr 's/.*:\t([0-9])/\1/p')
 
 if [ "$state" = "0" ]; then
-	xinput set-prop "$device" 140 1
+	xinput --enable "$device"
 else
-	xinput set-prop "$device" 140 0
+	xinput --disable "$device"
 fi
 

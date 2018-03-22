421d5a9ca2d050b13aa2efbdee1c0aee413bc260
Riyyi <riyyi3@gmail.com>
2018-03-22 12:50:11 +0100

    Update touchscreen script

--- .scripts/touchscreen.sh
+++ .scripts/touchscreen.sh
@@ -3,7 +3,7 @@
 device="04F3200A:00 04F3:2373"
 state=$(xinput --list-props "$device" | grep "Device Enabled" | sed -nr 's/.*:\t([0-9])/\1/p')
 
-if [ "$state" = "0" ]; then
+if [ "$state" = "0" ] && [ -z "$1" ]; then
 	xinput --enable "$device"
 else
 	xinput --disable "$device"

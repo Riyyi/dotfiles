a63df8967e434f7f940327d9ad2d1a7eb3c8e29e
Riyyi <riyyi3@gmail.com>
2018-10-21 15:15:31 +0200

    Change highlight color

--- .scripts/alias.sh
+++ .scripts/alias.sh
@@ -4,7 +4,7 @@ man() {
 	LESS_TERMCAP_mb=$'\e[01;31m' \
 	LESS_TERMCAP_md=$'\e[01;34m' \
 	LESS_TERMCAP_me=$'\e[0m' \
-	LESS_TERMCAP_so=$'\e[00;37m' \
+	LESS_TERMCAP_so=$'\e[01;107;30m' \
 	LESS_TERMCAP_se=$'\e[0m' \
 	LESS_TERMCAP_us=$'\e[04;95m' \
 	LESS_TERMCAP_ue=$'\e[0m' \

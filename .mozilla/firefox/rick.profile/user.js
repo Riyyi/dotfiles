ba77d3a41e2481cc11e8160d49b4a2ae85eab52f
Riyyi <riyyi3@gmail.com>
2018-10-25 15:51:06 +0200

    Add Firefox profile and config

---
+++ .mozilla/firefox/rick.profile/user.js
@@ -0,0 +1,6 @@
+// Display a blank new tab
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtab.url", "about:blank");
+
+// Fix dark GTK themes
+user_pref("widget.content.gtk-theme-override", "Arc");

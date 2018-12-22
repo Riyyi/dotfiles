63b8f763ef6d8282880fa9005d3db79d4532eafb
Riyyi <riyyi3@gmail.com>
2018-12-22 15:54:33 +0100

    Disable firefox tracking

--- .mozilla/firefox/rick.profile/user.js
+++ .mozilla/firefox/rick.profile/user.js
@@ -4,3 +4,15 @@ user_pref("browser.newtab.url", "about:blank");
 
 // Fix dark GTK themes
 user_pref("widget.content.gtk-theme-override", "Arc");
+
+// No full screen warning
+user_pref("full-screen-api.warning.timeout", 0);
+
+// Disable tracking
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
+user_pref("geo.enabled", false);

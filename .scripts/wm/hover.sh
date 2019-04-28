30ed143583d5dda79e0793a426f09bc90c0524a8
Riyyi <riyyi3@gmail.com>
2019-04-28 04:00:24 +0200

    Add window hover script, sticky floating window i3 keybind

---
+++ .scripts/wm/hover.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Enable mathematics in POSIX shell
+calc() { awk "BEGIN{print $*}"; }
+
+# Calculate size
+MON_WIDTH=$(xdotool getdisplaygeometry | awk '{print $1}')
+MON_HEIGHT=$(xdotool getdisplaygeometry | awk '{print $2}')
+NEW_WIDTH=$(calc "$MON_WIDTH * 0.25")
+NEW_HEIGHT=$(calc "$NEW_WIDTH / 16 * 9")
+
+# Calculate position
+BORDER=23
+X=$(calc "$MON_WIDTH - $NEW_WIDTH - $BORDER")
+Y=$(calc "$MON_HEIGHT - $NEW_HEIGHT - $BORDER")
+
+# Set window
+CURRENT=$(xdotool getwindowfocus)
+xdotool windowsize "$CURRENT" "$NEW_WIDTH" "$NEW_HEIGHT"
+xdotool windowmove "$CURRENT" "$X" "$Y"

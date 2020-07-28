379ac8665847c18979b080c80306fc2d85d82de8
Riyyi <riyyi3@gmail.com>
2020-07-28 22:34:45 +0200

    Cleanup .emacs.d and .cache/emacs directories

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,22 +12,6 @@
 
 ; --------------------------------------
 
-(defvar emacs-cache (concat (getenv "XDG_CACHE_HOME") "/emacs")
-  "Directory where Emacs cache data is stored.")
-(defvar emacs-d (concat (getenv "HOME") "/.emacs.d")
-  "Directory where Emacs config files are stored.")
-
-(defvar dot/leader-key "SPC"
-  "Leader prefix key.")
-(defvar dot/leader-alt-key "M-SPC"
-  "Alternative leader prefix key, used for Insert and Emacs states.")
-(defvar dot/localleader-key "SPC m"
-  "Local leader prefix key, for 'major-mode' specific commands.")
-(defvar dot/localleader-alt-key "M-SPC m"
-  "Alternative local leader prefix key, used for Insert and Emacs states.")
-
-; --------------------------------------
-
 ;; Add the melpa repository to the package manager
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -42,9 +26,12 @@
 
 ; --------------------------------------
 
+;; Base directory
+(setq user-emacs-directory (file-name-directory load-file-name))
+
 ;; Tangle configuration file
 (require 'org)
-(when (file-readable-p (concat emacs-d "/config.org"))
-  (org-babel-load-file (concat emacs-d "/config.org")))
+(when (file-readable-p (concat user-emacs-directory "config.org"))
+  (org-babel-load-file (concat user-emacs-directory "config.org")))
 
 ;;; init.el ends here

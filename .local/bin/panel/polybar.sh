98175e30ae8276eb2c414c8606ee5d3f41090e8b
Riyyi <riyyi3@gmail.com>
2021-08-30 21:02:22 +0200

    Script: Properly detach Polybar process

--- .local/bin/panel/polybar.sh
+++ .local/bin/panel/polybar.sh
@@ -2,10 +2,10 @@
 
 # Kill existing panels
 while [ "$(pgrep -cx polybar)" -gt 1 ]; do
-	pkill -x -9 polybar;
+	pkill --exact --signal 9 polybar
 done
 
 # Start a panel on each monitor
 for m in $(polybar --list-monitors | cut -d ":" -f1); do
-	MONITOR=$m polybar --reload polybar &
+	MONITOR="$m" setsid -f polybar --reload polybar
 done
